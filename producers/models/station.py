6fd9bd5cb4785752552d6fcd7be2eb6dd0938519
Kylie Pace <kylie.pace@playsportsnetwork.com>
2020-12-31 08:03:12 +0000

    turnstile producer emits event paired with avros schemas

--- producers/models/station.py
+++ producers/models/station.py
@@ -15,9 +15,6 @@ class Station(Producer):
     # avro.load or avro.loads?
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
 
-    #
-    # TODO: Define this value schema in `schemas/arrival_value.json, then uncomment the below
-    #
     value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
