052a199bea2eaa27151a7bc0ff6b49c9c2e47953
Kylie Pace <kylie.pace@playsportsnetwork.com>
2021-01-08 16:00:31 +0000

    subscribe to array of topics

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -56,7 +56,7 @@ class KafkaConsumer:
         # how the `on_assign` callback should be invoked.
         #
         #
-        self.consumer.subscribe(self.topic_name_pattern, on_assign=self.on_assign)
+        self.consumer.subscribe([self.topic_name_pattern], on_assign=self.on_assign)
 
     def on_assign(self, consumer, partitions):
         """Callback for when topic assignment takes place"""
